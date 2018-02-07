9e3262ba303eb534518cc825bc7611230b177849
TheNKetchum <nketchum.github@gmail.com>
2018-02-06 20:33:26 -0800

    Write function to invert colors. It works!

--- pr2.c
+++ pr2.c
@@ -5,8 +5,29 @@
 
 #define FILENAME_LIMIT 50
 #define HEADER_SIZE 54
-#define ELEM_SIZE 1
+#define ONE_BYTE 1
 #define COLORS_PER_PIXEL 3
+
+void invertColors(char *theHeader, int theWidth, unsigned char thePixels[][theWidth * COLORS_PER_PIXEL], int theHeight) {
+
+    FILE* outfile = fopen("InvertedColorCopy.bmp", "wb");
+    unsigned char newArray[theHeight][theWidth * COLORS_PER_PIXEL];
+    memcpy(newArray, thePixels, (size_t) (theHeight * theWidth * COLORS_PER_PIXEL));
+
+    int r, c;
+    for (r = 0; r < theHeight; r++) {
+        for (c = 0; c < (theWidth * COLORS_PER_PIXEL); c++/*<--ayy*/) {
+            unsigned char temp = newArray[r][c];
+            temp = (unsigned char) (255 - temp);
+            newArray[r][c] = temp;
+        }
+    }
+
+    fwrite(theHeader, ONE_BYTE, HEADER_SIZE, outfile);
+    fwrite(newArray, ONE_BYTE, (size_t) (theHeight * theWidth * COLORS_PER_PIXEL), outfile);
+
+    fclose(outfile);
+}
 
 int main() {
 
@@ -21,20 +42,27 @@ int main() {
     scanf("%d %d", &height, &width);
 
     FILE* infile = fopen(file, "rb");
-    FILE* outfile = fopen("copy1.bmp", "wb");
-    char header[54];
-    unsigned char pixels[height][width * 3];
+    char header[HEADER_SIZE];
+    unsigned char pixels[height][width * COLORS_PER_PIXEL];
+    fread(header, ONE_BYTE, HEADER_SIZE, infile);
+    fread(pixels, ONE_BYTE, (size_t) (height * width * COLORS_PER_PIXEL), infile);
 
-    fread(header, ELEM_SIZE, HEADER_SIZE, infile);
-    fread(pixels, ELEM_SIZE, (size_t) (height * width * COLORS_PER_PIXEL), infile);
+    invertColors(header, width, pixels, height);
 
-    fwrite(header, ELEM_SIZE, HEADER_SIZE, outfile);
-    fwrite(pixels, ELEM_SIZE, (size_t) (height * width * COLORS_PER_PIXEL), outfile);
+
+/*  FILE* outfile = fopen("copy1.bmp", "wb");
+
+
+    fwrite(header, ONE_BYTE, HEADER_SIZE, outfile);
+    fwrite(pixels, ONE_BYTE, (size_t) (height * width * COLORS_PER_PIXEL), outfile);
+
+
+    fclose(outfile);
+*/
 
     fclose(infile);
-    fclose(outfile);
-
     printf("Done. Check the generated images\n");
 
     return 0;
 }
+
